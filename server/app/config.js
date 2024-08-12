ef2eeccb114661096be2b8700067c42408d13fa4
Johnny Domingues <johnnydomingues@sfr.fr>
2024-08-12 15:16:55 +0200

    nettoyage + fin de config

--- server/app/config.js
+++ server/app/config.js
@@ -27,18 +27,15 @@ const path = require("path");
 // 4. Be sure to only have URLs in the array with domains from which you want to allow requests.
 // For example: ["http://mysite.com", "http://another-domain.com"]
 
-
 const cors = require("cors");
 
 app.use(
   cors({
     origin: [
       process.env.CLIENT_URL, // keep this one, after checking the value in `server/.env`
-      
-    ]
+    ],
   })
 );
-
 
 /* ************************************************************************* */
 
@@ -55,7 +52,7 @@ app.use(
 
 // Uncomment one or more of these options depending on the format of the data sent by your client:
 
- app.use(express.json())
+app.use(express.json());
 // app.use(express.urlencoded());
 // app.use(express.text());
 // app.use(express.raw());
@@ -108,7 +105,6 @@ app.use("/api", apiRouter);
 // 1. Uncomment the lines related to serving static files and redirecting unhandled requests.
 // 2. Ensure that the `reactBuildPath` points to the correct directory where your client's build artifacts are located.
 
-
 const reactBuildPath = path.join(__dirname, "/../../client/dist");
 const publicFolderPath = path.join(__dirname, "/../public");
 
@@ -125,7 +121,6 @@ app.get("*.*", express.static(publicFolderPath, { maxAge: "1y" }));
 app.get("*", (_, res) => {
   res.sendFile(path.join(reactBuildPath, "/index.html"));
 });
-
 
 /* ************************************************************************* */
 
