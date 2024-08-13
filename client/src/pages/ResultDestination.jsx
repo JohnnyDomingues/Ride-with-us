ef0d6ff38f90d93454c818a5e5c48a4a4ed9e796
Oresti <hoxhallarioresti02@gmail.com>
2024-08-13 18:15:27 +0200

    SIUUUUUUUUUUUUUUUUUUUUUU

--- client/src/pages/ResultDestination.jsx
+++ client/src/pages/ResultDestination.jsx
@@ -1,6 +1,24 @@
 
+import { Link } from "react-router-dom";
+
+import logo from "../assets/images/Logo-RWU.png";
+import "../Styles/resultDestination.css"
+import DestinationHere from "../components/DestinationHere";
+
 function ResultDestination() {
-  return <h1>Result of your research</h1>;
+  return (
+    <div>
+      <nav className="navigation-bar">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo" />
+        </Link>
+      </nav>
+      <div>
+        <h2 className="we-Found">We found them!!!</h2>
+      </div>
+      <DestinationHere/>
+    </div>
+  );
 }
 
 export default ResultDestination;
